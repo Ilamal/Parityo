310170e90c02b822dd812ea709e96a144153d84c
Ilamal <Ilari.s.Malinen@gmail.com>
2018-04-23 23:17:13 +0300

    Adding basics for front-end with react

---
+++ weather-app/src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import './App.css';
+let iter = 15;
+class App extends Component {
+  constructor(){
+    super();
+  }
+
+  render() {
+    return (
+      
+      <div className="App">
+  
+        <h1>Weather App</h1>  
+ 
+      </div>  
+    );
+  }
+}
+
+export default App;
