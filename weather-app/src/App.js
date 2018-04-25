a0f5584e71139d1922431832e1df29f65a13a17f
Ilamal <Ilari.s.Malinen@gmail.com>
2018-04-25 11:38:52 +0300

    working openweather api call

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,40 +1,51 @@
 import React, { Component } from 'react';
 import './App.css';
 import Title from './Components/Title'
-const key = '21a4b6e72379e58cb6cb2c11409903d6'
-var XMLParser = require('react-xml-parser');
-let place = "kuopio";
-var baseUrl = 'http://api.openweathermap.org/data/2.5/weather/';
-var kaupunki = 'q=Kuopio,fi';
-var unit = '&units=metric';
-var appid = '&appid=2f8b3222fce8d6f1b7fd7dff59724227'; 
-var wholeurl = baseUrl + '?' + kaupunki + appid + unit ;
-class App extends Component {
 
+let baseUrl = 'http://api.openweathermap.org/data/2.5/weather';
+let kaupunki = 'q=Kuopio,fi';
+let unit = '&units=metric';
+const appid = '&appid=2f8b3222fce8d6f1b7fd7dff59724227'; 
+let wholeurl = baseUrl + '?' + kaupunki + unit + appid ;
+let httpreq = new XMLHttpRequest();
+var self;
+class App extends Component {
+  
   constructor(){
     super();
-    var httpreq = new XMLHttpRequest();
-    httpreq.onreadystatechange = (function (){
-   //   let tulos = JSON.parse(httpreq.responseText);
-    let tulos = httpreq.responseText;
-      console.log(tulos);
-    }
-    );
-  httpreq.open('GET', wholeurl, true);
-  httpreq.send();
-}
- 
+
+    this.state = {temp: 0};
+    self = this;
+    httpreq.onreadystatechange = (function(){
+      try{
+        let tulos = JSON.parse(this.responseText);
+        console.log(tulos.main.temp);
+        self.setState({temp: tulos.main.temp});
+      //  this.state = {temp: {temp}};
+      } catch(Exception){
+
+      }
+    });
     
+    
+}
+  update
+  callApi(){
+    httpreq.open('GET', wholeurl, true);
+    httpreq.send();
+   
+  }
 
   render() {
-    setTimeout(()=> {
-      this.setState({weather: "rain"});
-    }, 1000)
+   
     return (
       
       <div className="App">
         <Title />
-
+        <button onClick={this.callApi} >
+          callApi
+        </button>
+        {this.state.temp}
       </div>  
     );
   }
---
+++ weather-app/src/Components/Handel.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class Handel extends Component {
+
+  render() {
+    return (
+        <h1>Weather-app to go!</h1>
+    );
+  }
+}
+export default Handel;
